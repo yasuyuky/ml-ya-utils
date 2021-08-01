40b6403e560365956fae19a7127d03465c1c940a
yasuyuky <yasuyuki.ymd@gmail.com>
2021-08-01 15:57:49 +0900

    Add backup.py

---
+++ ml/ya/utils/backup.py
@@ -0,0 +1,74 @@
+import ast
+import glob
+import inspect
+import json
+import os
+import shutil
+import sys
+
+
+class SourceBackup:
+    def __init__(self, backupdir="output"):
+        self.backupdir = backupdir
+        root = inspect.stack()[-1][1]
+        self.backup_file(root)
+        self.retrieve_file(root)
+
+    def retrieve_file(self, filepath):
+        try:
+            source = open(filepath).read()
+        except Exception as e:
+            print(e, file=sys.stderr)
+            return
+        tree = ast.parse(source)
+        for name in self.retrieve_ast(tree):
+            if not name: continue
+            name_seq = name.split('.')
+            path = os.path.join(os.path.dirname(filepath), *name_seq)
+            if os.path.exists(path + '.py'):
+                self.backup_file(path + '.py')
+                self.retrieve_file(path + '.py')
+            elif os.path.isdir(path):
+                for subpath in glob.glob(os.path.join(path, '*')):
+                    self.retrieve_file(subpath)
+
+    def retrieve_ast(self, nodes):
+        for node in ast.iter_child_nodes(nodes):
+            if isinstance(node, ast.Import):
+                yield self.get_import(ast.iter_fields(node))
+            elif isinstance(node, ast.ImportFrom):
+                yield self.get_import(ast.iter_fields(node))
+            else:
+                self.retrieve_ast(node)
+
+    def get_import(self, nodes):
+        d = dict(nodes)
+        if 'module' in d:
+            return d['module']
+        elif 'names' in d:
+            for k, v in ast.iter_fields(d['names'][0]):
+                if k == 'name':
+                    return v
+
+    def backup_file(self, f):
+        dst = os.path.join(self.backupdir, f)
+        if not os.path.exists(os.path.dirname(dst)):
+            os.makedirs(os.path.dirname(dst))
+        os.chmod(os.path.dirname(dst), 0o755)
+        shutil.copyfile(f, dst)
+
+
+class ArgumentBackup:
+    def __init__(self, args, filename='args.json', backupdir="output"):
+        self.args = args
+        self.filename = filename
+        self.backupdir = backupdir
+        self.store_args()
+
+    def store_args(self):
+        with open(os.path.join(self.backupdir, self.filename), 'w') as f:
+            if not os.path.exists(self.backupdir):
+                os.makedirs(self.backupdir)
+            args_dict = vars(self.args)
+            args = {k: args_dict[k] for k in args_dict}
+            json.dump(args, f)
